5dbf46e9030c8d745da1a5b9b30f57eb7dd8c524
effortlessdevsec <amit@payatu.io>
2023-08-01 18:19:17 +0530

    text

--- app/src/main/java/com/BugBazaar/ui/MyProfile.java
+++ app/src/main/java/com/BugBazaar/ui/MyProfile.java
@@ -1,8 +1,5 @@
 package com.BugBazaar.ui;
 
-import static com.BugBazaar.R.*;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,12 +8,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.BugBazaar.Models.Profile;
 import com.BugBazaar.utils.DeviceDetails;
 import com.BugBazaar.R;
 import com.google.firebase.FirebaseApp;
@@ -27,124 +21,112 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
-        public class MyProfile extends AppCompatActivity {
-            private FirebaseStorage firebaseStorage;
+public class MyProfile extends AppCompatActivity {
+    private FirebaseStorage firebaseStorage;
 
-private EditText editText;
-            private Profile userProfile;
-            private EditText etName, etEmail, etMobileNumber;
-            private static final int SELECT_PHOTO_REQUEST = 1;
-            private ImageView imageView;
 
-            @SuppressLint("MissingInflatedId")
+    private static final int SELECT_PHOTO_REQUEST = 1;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myprofile);
+        FirebaseApp.initializeApp(this);
+        firebaseStorage = FirebaseStorage.getInstance();
+        imageView = findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(layout.activity_myprofile);
-                FirebaseApp.initializeApp(this);
-                firebaseStorage = FirebaseStorage.getInstance();
-
-//// You can implement this method to load data from SharedPreferences or any other source.
-
-
-
-
-
-                imageView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        selectPhotoFromGallery();
-                    }
-                });
-
-
-
-                // Load and display the image from internal storage
-                loadAndDisplayImage();
+            public void onClick(View v) {
+                selectPhotoFromGallery();
             }
+        });
 
 
 
-            private void selectPhotoFromGallery() {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, SELECT_PHOTO_REQUEST);
-            }
+        // Load and display the image from internal storage
+        loadAndDisplayImage();
+    }
 
-            @Override
-            protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-                super.onActivityResult(requestCode, resultCode, data);
+    private void selectPhotoFromGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        photoPickerIntent.setType("*/*");
+        startActivityForResult(photoPickerIntent, SELECT_PHOTO_REQUEST);
+    }
 
-                if (requestCode == SELECT_PHOTO_REQUEST && resultCode == Activity.RESULT_OK) {
-                    if (data != null) {
-                        try {
-                            Uri selectedImageUri = data.getData();
-                            InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
-                            Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
-                            imageView.setImageBitmap(imageBitmap);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-                            // Save the image to internal storage
-                            saveImageToInternalStorage(imageBitmap);
-                            uploadImageToFirebaseStorage(selectedImageUri);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-
-
-
-            private void uploadImageToFirebaseStorage(Uri imageUri) {
-               String device= DeviceDetails.getDeviceName();
-                // Get a reference to the Firebase Storage location where you want to upload the image
-                StorageReference storageRef = firebaseStorage.getReference().child(device+"/" + System.currentTimeMillis() + ".png");
-
-                // Upload the image
-                storageRef.putFile(imageUri)
-                        .addOnSuccessListener(taskSnapshot -> {
-                            Log.d("hello","success");
-                            // Image upload successful, do something if needed
-                        })
-                        .addOnFailureListener(exception -> {
-                            Log.d("hello","fail");
-                            // Handle unsuccessful uploads, do something if needed
-                        });
-            }
-
-
-
-            private void loadAndDisplayImage() {
+        if (requestCode == SELECT_PHOTO_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
                 try {
-                    String filename = "my_image.png";
-                    File directory = getFilesDir();
-                    File file = new File(directory, filename);
+                    Uri selectedImageUri = data.getData();
+                    InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
+                    Bitmap imageBitmap = BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(imageBitmap);
 
-                    if (file.exists()) {
-                        Bitmap imageBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-                        imageView.setImageBitmap(imageBitmap);
-                    }
+                    // Save the image to internal storage
+                    saveImageToInternalStorage(imageBitmap);
+                    uploadImageToFirebaseStorage(selectedImageUri);
+
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-
-            private void saveImageToInternalStorage(Bitmap imageBitmap) {
-                try {
-                    String filename = "my_image.png";
-                    File directory = getFilesDir();
-                    File file = new File(directory, filename);
-
-                    FileOutputStream outputStream = new FileOutputStream(file);
-                    imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-                    outputStream.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-
         }
+    }
+
+
+
+    private void uploadImageToFirebaseStorage(Uri imageUri) {
+        String device= DeviceDetails.getDeviceName();
+        // Get a reference to the Firebase Storage location where you want to upload the image
+        StorageReference storageRef = firebaseStorage.getReference().child(device+"/" + System.currentTimeMillis() + ".png");
+
+        // Upload the image
+        storageRef.putFile(imageUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    Log.d("hello","success");
+                    // Image upload successful, do something if needed
+                })
+                .addOnFailureListener(exception -> {
+                    Log.d("hello","fail");
+                    // Handle unsuccessful uploads, do something if needed
+                });
+    }
+
+
+
+    private void loadAndDisplayImage() {
+        try {
+            String filename = "my_image.png";
+            File directory = getFilesDir();
+            File file = new File(directory, filename);
+
+            if (file.exists()) {
+                Bitmap imageBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                imageView.setImageBitmap(imageBitmap);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveImageToInternalStorage(Bitmap imageBitmap) {
+        try {
+            String filename = "my_image.png";
+            File directory = getFilesDir();
+            File file = new File(directory, filename);
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+            imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
