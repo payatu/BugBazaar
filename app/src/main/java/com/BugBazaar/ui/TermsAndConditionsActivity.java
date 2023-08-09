ee575ce845bc37b6f3d9e4edfabb8a5e03ac07f9
banditVedant <vedant@payatu.io>
2023-08-09 21:22:56 +0530

    1. Navigation UI drawer is added. 2. Each activity is mapped with navigation drawer items. (Added java code) 3. Customized toolbar for each activity is added. 4. Theme color changed. 5. More items added in navigation item list.

--- app/src/main/java/com/BugBazaar/ui/TermsAndConditionsActivity.java
+++ app/src/main/java/com/BugBazaar/ui/TermsAndConditionsActivity.java
@@ -3,6 +3,7 @@ package com.BugBazaar.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,6 +11,7 @@ import android.webkit.WebViewClient;
 import com.BugBazaar.R;
 import com.BugBazaar.utils.AppConstants;
 import android.webkit.SafeBrowsingResponse;
+import android.widget.TextView;
 
 public class TermsAndConditionsActivity extends AppCompatActivity {
     WebView webView;
@@ -21,6 +23,10 @@ public class TermsAndConditionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_and_conditions);
         webView = findViewById(R.id.terms_condition_view);
+
+        //Toolbar title set
+        TextView toolbarTitle = findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText("Terms & Conditions");
 
         setupwebview(webView);
         if (getIntent().getExtras() != null) {
@@ -67,5 +73,9 @@ public class TermsAndConditionsActivity extends AppCompatActivity {
         }
 
 
+    }
+    //Code to handle backbutton
+    public void onBackButtonClick(View view) {
+        onBackPressed(); // Navigate back to the previous activity
     }
 }
