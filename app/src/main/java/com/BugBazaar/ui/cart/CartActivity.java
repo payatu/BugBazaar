afcb6c15ec7931b456cd5c69719c251d6bf8f386
banditVedant <vedant@payatu.io>
2023-09-06 13:44:21 +0530

    1. Items are getting added multiple times whenever we started CartActivity has been fixed.

--- app/src/main/java/com/BugBazaar/ui/cart/CartActivity.java
+++ app/src/main/java/com/BugBazaar/ui/cart/CartActivity.java
@@ -1,5 +1,4 @@
 package com.BugBazaar.ui.cart;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,9 +12,7 @@ import android.widget.TextView;
 import com.BugBazaar.R;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -28,7 +25,7 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maincart);
 
-        CartDatabaseHelper cartDBHelper=new CartDatabaseHelper(this,"cart.db",null,1);
+        CartDatabaseHelper cartDBHelper = new CartDatabaseHelper(this, "cart.db", null, 1);
 
         TextView toolbarTitle = findViewById(R.id.toolbarTitle);
         toolbarTitle.setText("My Cart");
@@ -36,45 +33,14 @@ public class CartActivity extends AppCompatActivity {
         // Find the RecyclerView in the layout
         cartRecyclerView = findViewById(R.id.cartRecyclerView);
 
-        // Initialize your cartItems list and populate it
-        cartItems = new ArrayList<>();
-
-        // Retrieve the ArrayList<CartItem> extra from the intent
-        Intent intent = getIntent();
-        ArrayList<CartItem> receivedCartItems = intent.getParcelableArrayListExtra("cartItems");
-
-        // Log the product names from the initial cartItems list
-        // Adding new cart items (receivedCartItems) to old list (cartItems)
-        if (receivedCartItems != null) {
-            for (CartItem cartItem : receivedCartItems) {
-                // Check if the item already exists in cartItems
-                boolean itemExists = false;
-                for (CartItem existingItem : cartItems) {
-                    if (existingItem.getProductName().equals(cartItem.getProductName())) {
-                        // If the item exists, update its quantity
-                        int existingQuantity = existingItem.getQuantity();
-                        existingItem.setQuantity(existingQuantity + cartItem.getQuantity());
-                        itemExists = true;
-                        // Save the product details to the SQLite database
-
-                        break;
-                    }
-                }
-
-                if (!itemExists) {
-                    // If the item is not already in cartItems, add it
-                    cartItems.add(cartItem);
-                }
-            }
-        }
+        // Initialize your cartItems list and populate it with all items from the database
+        cartItems = cartDBHelper.getAllRecords();
 
         // Create and set up the adapter
         cartAdapter = new CartAdapter(this, cartItems);
 
         cartRecyclerView.setAdapter(cartAdapter);
         cartRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        // Notify the adapter that the dataset has changed
-        cartAdapter.notifyDataSetChanged();
     }
 
     // Code to handle back button
